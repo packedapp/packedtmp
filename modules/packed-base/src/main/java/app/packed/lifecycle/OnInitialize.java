b92ae8a6d70b15428a1e4aa19e4563b8e7d5f448
Kasper Nielsen <kasperni@gmail.com>
2018-11-28 10:21:24 +0000

    qwe

--- modules/packed-base/src/main/java/app/packed/lifecycle/OnInitialize.java
+++ modules/packed-base/src/main/java/app/packed/lifecycle/OnInitialize.java
@@ -20,6 +20,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import app.packed.inject.Inject;
 import app.packed.inject.Injector;
 
 /**
