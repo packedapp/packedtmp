6d48197caaf3d71944652a43c6d59df97fb30ea1
Kasper Nielsen <kasperni@gmail.com>
2019-10-23 06:11:22 +0700

    wer

--- modules/packed-base/src/main/java/packed/internal/hook/model/OnHookContainerModel.java
+++ modules/packed-base/src/main/java/packed/internal/hook/model/OnHookContainerModel.java
@@ -17,11 +17,11 @@ package packed.internal.hook.model;
 
 import static java.util.Objects.requireNonNull;
 
+import java.lang.annotation.Annotation;
 import java.lang.invoke.MethodHandle;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
 import java.util.IdentityHashMap;
-import java.util.List;
 import java.util.Map;
 
 import app.packed.hook.AnnotatedFieldHook;
@@ -31,9 +31,13 @@ import app.packed.hook.AssignableToHook;
 import app.packed.hook.Hook;
 import app.packed.hook.OnHook;
 import app.packed.lang.Nullable;
+import packed.internal.hook.HookProcessor;
 import packed.internal.hook.model.OnHookContainerModelBuilder.LinkedEntry;
 import packed.internal.hook.model.OnHookContainerModelBuilder.OnHookContainerNode;
+import packed.internal.moduleaccess.ModuleAccess;
+import packed.internal.reflect.ClassProcessor;
 import packed.internal.util.ThrowableUtil;
+import packed.internal.util.UncheckedThrowableFactory;
 
 /**
  *
@@ -41,7 +45,7 @@ import packed.internal.util.ThrowableUtil;
 public class OnHookContainerModel {
 
     /** Methods annotated with {@link OnHook} that takes a {@link AnnotatedFieldHook} as a parameter. */
-    final Map<Class<?>, Link> onHookAnnotatedFields;
+    private final Map<Class<?>, Link> onHookAnnotatedFields;
 
     /** Methods annotated with {@link OnHook} that takes a {@link AnnotatedMethodHook} as a parameter. */
     final Map<Class<?>, Link> onHookAnnotatedMethods;
@@ -53,9 +57,10 @@ public class OnHookContainerModel {
     final Map<Class<?>, Link> onHookAssignableTos;
 
     /** Methods annotated with {@link OnHook} that takes a non-base {@link Hook}. */
-    final Link[] onHookCustomHooks;
+    private final Link[] onHookCustomHooks;
 
-    final List<MethodHandle> constructors;
+    /** Constructors for each builder. */
+    private final MethodHandle[] constructors;
 
     OnHookContainerModel(OnHookContainerModelBuilder b) {
         ArrayList<MethodHandle> nodes = new ArrayList<>();
@@ -67,14 +72,53 @@ public class OnHookContainerModel {
         this.onHookAnnotatedTypes = convert(b.onHookAnnotatedTypes);
         this.onHookAssignableTos = convert(b.onHookAssignableTos);
         Map<Class<?>, Link> tmp = convert(b.onHookCustomHooks);
-        onHookCustomHooks = new Link[b.sorted.size()];
-        if (tmp != null) {
-            for (int i = 0; i < b.sorted.size(); i++) {
-                Class<? extends Hook> cl = b.sorted.get(i).hookType;
-                onHookCustomHooks[i] = tmp.get(cl);
+
+        this.onHookCustomHooks = new Link[b.sorted.size()];
+        this.constructors = new MethodHandle[b.sorted.size()];
+        for (int i = 0; i < b.sorted.size(); i++) {
+            OnHookContainerNode n = b.sorted.get(i);
+            constructors[i] = n.constructor;
+            if (tmp != null) {
+                onHookCustomHooks[i] = tmp.get(n.hookType);
             }
         }
-        this.constructors = List.copyOf(nodes);
+    }
+
+    public Object process(ClassProcessor cpTarget, UncheckedThrowableFactory<?> tf) {
+        Object[] array = new Object[constructors.length];
+        HookProcessor hc = new HookProcessor(cpTarget, tf);
+        cpTarget.findMethodsAndFields(c -> {}, onHookAnnotatedFields == null ? null : f -> {
+            for (Annotation a : f.getAnnotations()) {
+                for (Link link = onHookAnnotatedFields.get(a.annotationType()); link != null; link = link.next) {
+                    Object builder = link.builderOf(this, array);
+                    AnnotatedFieldHook<Annotation> hook = ModuleAccess.hook().newAnnotatedFieldHook(hc, f, a);
+                    try {
+                        link.mh.invoke(builder, hook);
+                    } catch (Throwable e) {
+                        ThrowableUtil.rethrowErrorOrRuntimeException(e);
+                        throw new UndeclaredThrowableException(e);
+                    }
+                }
+            }
+        });
+        hc.close();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            Object h = array[i];
+            if (h != null) {
+                array[i] = ((Hook.Builder<?>) h).build();
+                for (Link link = onHookCustomHooks[i]; link != null; link = link.next) {
+                    Object builder = link.builderOf(this, array);
+                    try {
+                        link.mh.invoke(builder, array[i]);
+                    } catch (Throwable e1) {
+                        ThrowableUtil.rethrowErrorOrRuntimeException(e1);
+                        throw new UndeclaredThrowableException(e1);
+                    }
+                }
+            }
+        }
+        return array[0];
     }
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
@@ -89,7 +133,7 @@ public class OnHookContainerModel {
             OnHookContainerModelBuilder.LinkedEntry e = (LinkedEntry) v;
             Link l = null;
             for (; e != null; e = e.next) {
-                l = new Link(e.methodHandle, e.builder.id, l);
+                l = new Link(e, l);
             }
             return l;
         });
@@ -97,24 +141,24 @@ public class OnHookContainerModel {
         return Map.copyOf(m);
     }
 
-    static class Link {
-        final MethodHandle mh;
-        final int index;
+    private static class Link {
+        private final MethodHandle mh;
+        private final int index;
 
         @Nullable
-        final Link next;
+        private final Link next;
 
-        Link(MethodHandle mh, int index, @Nullable Link next) {
-            this.mh = requireNonNull(mh);
-            this.index = index;
+        private Link(LinkedEntry e, @Nullable Link next) {
+            this.mh = requireNonNull(e.methodHandle);
+            this.index = e.builder.id;
             this.next = next;
         }
 
-        Object builderOf(OnHookContainerModel m, Object[] array) {
+        private Object builderOf(OnHookContainerModel m, Object[] array) {
             Object builder = array[index];
             if (builder == null) {
                 try {
-                    builder = array[index] = m.constructors.get(index).invoke();
+                    builder = array[index] = m.constructors[index].invoke();
                 } catch (Throwable e2) {
                     ThrowableUtil.rethrowErrorOrRuntimeException(e2);
                     throw new UndeclaredThrowableException(e2);
--- modules/packed-base/src/main/java/packed/internal/hook/model/UseIt2.java
+++ modules/packed-base/src/main/java/packed/internal/hook/model/UseIt2.java
@@ -17,17 +17,10 @@ package packed.internal.hook.model;
 
 import static java.util.Objects.requireNonNull;
 
-import java.lang.annotation.Annotation;
 import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.UndeclaredThrowableException;
 
-import app.packed.hook.AnnotatedFieldHook;
 import app.packed.hook.Hook;
-import packed.internal.hook.HookProcessor;
-import packed.internal.hook.model.OnHookContainerModel.Link;
-import packed.internal.moduleaccess.ModuleAccess;
 import packed.internal.reflect.ClassProcessor;
-import packed.internal.util.ThrowableUtil;
 import packed.internal.util.UncheckedThrowableFactory;
 
 /**
@@ -46,45 +39,9 @@ public class UseIt2 {
         ohs.process();
 
         OnHookContainerModel m = new OnHookContainerModel(ohs);
-
         ClassProcessor cpTarget = new ClassProcessor(caller, target, false);
 
-        Object[] array = new Object[ohs.sorted.size()];
-
-        HookProcessor hc = new HookProcessor(cpTarget, UncheckedThrowableFactory.ASSERTION_ERROR);
-        cpTarget.findMethodsAndFields(c -> {}, m.onHookAnnotatedFields == null ? null : f -> {
-            for (Annotation a : f.getAnnotations()) {
-                for (Link link = m.onHookAnnotatedFields.get(a.annotationType()); link != null; link = link.next) {
-                    Object builder = link.builderOf(m, array);
-                    AnnotatedFieldHook<Annotation> hook = ModuleAccess.hook().newAnnotatedFieldHook(hc, f, a);
-                    try {
-                        link.mh.invoke(builder, hook);
-                    } catch (Throwable e) {
-                        ThrowableUtil.rethrowErrorOrRuntimeException(e);
-                        throw new UndeclaredThrowableException(e);
-                    }
-                }
-            }
-        });
-        hc.close();
-
-        for (int i = array.length - 1; i >= 0; i--) {
-            Object h = array[i];
-            if (h != null) {
-                array[i] = ((Hook.Builder<?>) h).build();
-                for (Link link = m.onHookCustomHooks[i]; link != null; link = link.next) {
-                    Object builder = link.builderOf(m, array);
-                    try {
-                        link.mh.invoke(builder, array[i]);
-                    } catch (Throwable e1) {
-                        ThrowableUtil.rethrowErrorOrRuntimeException(e1);
-                        throw new UndeclaredThrowableException(e1);
-                    }
-                }
-            }
-        }
-        return (T) array[0];
-
+        return (T) m.process(cpTarget, UncheckedThrowableFactory.ASSERTION_ERROR);
     }
 
 }
