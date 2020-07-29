418b45b1c2a5052c07d0da0b4c1e87fc39231a13
kasperni <kasperni@kaspers-mbp.lan>
2020-07-29 09:48:44 +0100

    qweqwe

---
+++ modules/packed-base/src/main/java/packed/internal/component/PackedSingletonConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2008 Kasper Nielsen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package packed.internal.component;
+
+import app.packed.component.AbstractComponentConfiguration;
+import app.packed.component.SingletonConfiguration;
+
+/**
+ *
+ */
+public class PackedSingletonConfiguration<T> extends AbstractComponentConfiguration implements SingletonConfiguration<T> {
+
+    private final PackedSingletonConfigurationContext<T> context;
+
+    public PackedSingletonConfiguration(PackedSingletonConfigurationContext<T> context) {
+        super(context);
+        this.context = context;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public PackedSingletonConfiguration<T> setDescription(String description) {
+        context.setDescription(description);
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public PackedSingletonConfiguration<T> setName(String name) {
+        context.setName(name);
+        return this;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String initializeNameDefaultName() {
+        return context.initializeNameDefaultName();
+    }
+}
