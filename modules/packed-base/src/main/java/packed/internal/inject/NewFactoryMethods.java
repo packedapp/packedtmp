0f2cbd0f127b0a3d445dc7dee8ac7be49694abc4
kasperni <kasperni@kaspers-mbp.lan>
2020-10-03 20:22:12 +0100

    qweqwe

--- modules/packed-base/src/main/java/packed/internal/inject/NewFactoryMethods.java
+++ modules/packed-base/src/main/java/packed/internal/inject/NewFactoryMethods.java
@@ -176,4 +176,33 @@ class NewFactoryMethods {
         throw new UnsupportedOperationException();
     }
 
+    static <T> Factory<T> findInstanceMethod(Object onInstance, Class<T> returnType) {
+        throw new UnsupportedOperationException();
+    }
+
+    static <T> Factory<T> findInstanceMethod(Object onInstance, Class<T> returnType, String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Man kunne ogsaa bare sige man tog instance metoder...
+    // Man saa skal man binde receiveren.....
+    //// Dvs for instans metoder, saa bliver selve instancen en dependency...
+    public static <T> Factory<T> fromMethodInstance(Object onInstance, Method method, Class<T> returnType) {
+        requireNonNull(returnType, "returnType is null");
+        return fromMethodInstance(onInstance, method, TypeLiteral.of(returnType));
+    }
+
+    public static <T> Factory<T> fromMethodInstance(Object onInstance, Method method, TypeLiteral<T> returnType) {
+        requireNonNull(method, "method is null");
+        requireNonNull(returnType, "returnType is null");
+        // ClassMirror mirror = ClassMirror.fromImplementation(method.getDeclaringClass());
+        // return new Factory<T>(new InternalFactory.fromExecutable<T>((Key<T>) mirror.getKey().ofType(returnType), mirror,
+        // Map.of(), new MethodMirror(method)));
+        throw new UnsupportedOperationException();
+    }
+
+    // How we skal have
+    public static <T> Factory<T> fromMethodInstance(Object onInstance, TypeLiteral<T> returnType, String name, Class<?>... parameterTypes) {
+        throw new UnsupportedOperationException();
+    }
 }
