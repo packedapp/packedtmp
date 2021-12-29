77983391c174a6db5fd01fbf3a48fe60604181b9
Kasper Nielsen <kasperni@gmail.com>
2021-12-29 20:30:11 +0000

    234

--- modules/packed/src/main/java/app/packed/extension/sandbox/connect/MyExt.java
+++
@@ -1,7 +0,0 @@
-package app.packed.extension.sandbox.connect;
-
-import app.packed.extension.Extension;
-
-public class MyExt extends Extension<MyExt> {
-
-}
--- modules/packed/src/main/java/app/packed/extension/sandbox/connect/MyExtBean.java
+++
@@ -1,7 +0,0 @@
-package app.packed.extension.sandbox.connect;
-
-import app.packed.extension.ExtensionBean;
-
-public class MyExtBean extends ExtensionBean {
-
-}
