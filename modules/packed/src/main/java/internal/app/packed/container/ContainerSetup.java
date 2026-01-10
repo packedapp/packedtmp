653c69b212242dc834869632b67bfef3050ba695
Kasper Nielsen <kasperni@gmail.com>
2026-01-10 18:50:21 +0100

    Update ContainerSetup.java

--- modules/packed/src/main/java/internal/app/packed/container/ContainerSetup.java
+++ modules/packed/src/main/java/internal/app/packed/container/ContainerSetup.java
@@ -34,7 +34,6 @@ import app.packed.container.Wirelet;
 import app.packed.extension.BaseExtension;
 import app.packed.extension.Extension;
 import app.packed.extension.ExtensionHandle;
-import app.packed.extension.InternalExtensionException;
 import app.packed.operation.OperationHandle;
 import app.packed.util.Nullable;
 import internal.app.packed.application.ApplicationSetup;
